1668e3df0494b1d512a7d1594684754d0ba33ae4
emilyrschwarz <emilyrschwarz99@gmail.com>
2019-09-16 19:20:29 -0400

    Added style guides and bug finder Started doc process

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * @author Emily R Schwarz
+ */
+
 public class Main extends Application {
+
+    /**
+     * The start method is the starting point of a JavaFX program. This starts method sets the title.
+     * @brief
+     * @param primaryStage The primary stage.
+     * @throws Exception Any problem with the code.
+     */
 
     @Override
     public void start(Stage primaryStage) throws Exception{
