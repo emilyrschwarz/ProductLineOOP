6f94b952a5cbd88e08acbba194c19bc0ceea7fcd
emilyrschwarz <emilyrschwarz99@gmail.com>
2019-09-29 14:14:23 -0400

    Connected Scenebuilder

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,68 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class Controller {
+
+  private Connection conn;
+
+  static final String JDBC_DRIVER = "org.h2.Driver";
+  static final String DB_URL = "jdbc:h2:./res/ProductLineDatabase";
+
+
+  @FXML ComboBox<Integer> produceQuantityComboBox;
+
+  @FXML
+  void addProductButtonClick(ActionEvent event) {
+
+    try (Statement statement = conn.createStatement()) {
+
+      statement.execute(
+          "INSERT INTO Product(type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' );");
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @FXML
+  void recordProdButtonClick(ActionEvent event) {
+
+    System.out.println("Production Button");
+  }
+
+  void initialize() {
+
+    // intelliJ must be disconnected from database in order for program to connect
+
+    //  Database credentials
+    conn = null;
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      // STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL);
+
+      // STEP 3: Execute a query
+
+      // STEP 4: Clean-up environment
+      conn.close();
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
+
+    produceQuantityComboBox.getItems().addAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    produceQuantityComboBox.setEditable(true);
+
+    produceQuantityComboBox.getSelectionModel().selectFirst();
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,28 +7,27 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * @author Emily R Schwarz
- */
-
+ * This is the main class. It opens the JavaFX application.
+ *  @author Emily R Schwarz
+ *  */
 public class Main extends Application {
 
-    /**
-     * The start method is the starting point of a JavaFX program. This starts method sets the title.
-     * @brief
-     * @param primaryStage The primary stage.
-     * @throws Exception Any problem with the code.
-     */
+  /**
+   * The start method is the starting point of a JavaFX program. This starts method sets the title.
+   *
+   * @brief
+   * @param primaryStage The primary stage.
+   * @throws Exception Any problem with the code.
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Product Line");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
